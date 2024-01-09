ac2caac27482e3480451d3b9b59e83ce05098d87
Colin Metcalf <colinmetcalf@outlook.com>
2024-01-08 19:16:05 -0800

    Add Drivetrain Constants

---
+++ src/main/java/frc/robot/constants/drivetrain/DrivetrainConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.constants.drivetrain;
+
+public class DrivetrainConstants 
+{
+    public static final double MAX_SPEED = 6; // meters per second
+    public static final double MAX_ANGULAR_RATE = Math.PI; // rads per second
+
+}
