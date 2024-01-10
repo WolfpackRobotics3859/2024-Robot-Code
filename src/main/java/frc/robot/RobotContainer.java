b23e5e20fe0dadbf6efec72ad51f424de32ce126
Colin Metcalf <96217596+coolinm@users.noreply.github.com>
2024-01-09 18:28:40 -0800

    2: Implement Drivetrain Subsystem
    * Implement Drivetrain Subsystem
    
    * Update Field Relative Seed
    
    * Remove Telemetry
    
    * Remove Field Relative Seeding
    
    * Add Drivetrain Constants
    
    * Move Drivetrain Declaration
    
    * Documentation
    
    * Documentation
    
    * Add Commands
    
    * Update Robot Container
    
    * Documentation
    
    * Fix syntax
    
    * Move Drivetrain To Parameter
    Change Class Capitalization
    
    * Fix syntax
    
    * Update local variables
    
    * Fix Field Relative
    
    * Add Drivetrain Getter
    
    * Rename Drive.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,15 +6,43 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.constants.drivetrain.TunerConstants;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.commands.drivetrain.Drive;
+import frc.robot.commands.drivetrain.SeedFieldRelative;
 
-public class RobotContainer {
-  public RobotContainer() {
+public class RobotContainer 
+{
+  private final Drivetrain m_Drivetrain = new Drivetrain(TunerConstants.DRIVETRAIN_CONSTANTS, TunerConstants.FRONT_LEFT,
+                      TunerConstants.FRONT_RIGHT, TunerConstants.BACK_LEFT, TunerConstants.BACK_RIGHT);
+
+  private final CommandXboxController primaryController = new CommandXboxController(0);
+
+  public Drivetrain getDriveSub()
+  {
+    return this.m_Drivetrain;
+  }
+  
+  private void configureBindings() 
+  {
+    m_Drivetrain.setDefaultCommand(
+      new Drive(m_Drivetrain,
+                () -> -primaryController.getLeftY(),
+                () -> -primaryController.getLeftX(),
+                () -> -primaryController.getRightX()
+      ));
+
+    primaryController.a().onTrue(new SeedFieldRelative(m_Drivetrain));
+  }
+
+  public RobotContainer() 
+  {
     configureBindings();
   }
 
-  private void configureBindings() {}
-
-  public Command getAutonomousCommand() {
+  public Command getAutonomousCommand() 
+  {
     return Commands.print("No autonomous command configured");
   }
 }
---
+++ src/main/java/frc/robot/constants/drivetrain/DrivetrainConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants.drivetrain;
+
+public class DrivetrainConstants 
+{
+    /** Meters per second */
+    public static final double MAX_SPEED = 6;
+
+    /** Radians per second */
+    public static final double MAX_ANGULAR_RATE = Math.PI;
+
+}
