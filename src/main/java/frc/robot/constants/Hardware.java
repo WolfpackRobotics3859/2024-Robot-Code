4b708d54323d249c2fd5c9d0fa58bd7fbbc7a21f
Colin Metcalf <colinmetcalf@outlook.com>
2024-01-11 00:05:42 -0800

    Create Constants

---
+++ src/main/java/frc/robot/constants/Hardware.java
@@ -0,0 +1,29 @@
+package frc.robot.constants;
+
+public class Hardware
+{
+    // Front Left Swerve Module
+    public static final int FRONT_LEFT_DRIVE_MOTOR_ID = 1;
+    public static final int FRONT_LEFT_STEER_MOTOR_ID = 2;
+    public static final int FRONT_LEFT_ENCODER_ID = 1;
+
+    // Front Right Swerve Module
+    public static final int FRONT_RIGHT_DRIVE_MOTOR_ID = 3;
+    public static final int FRONT_RIGHT_STEER_MOTOR_ID = 4;
+    public static final int FRONT_RIGHT_ENCODER_ID = 2;
+
+    // Back Left Swerve Module
+    public static final int BACK_LEFT_DRIVE_MOTOR_ID = 5;
+    public static final int BACK_LEFT_STEER_MOTOR_ID = 6;
+    public static final int BACK_LEFT_ENCODER_ID = 3;
+
+    // Back Right Swerve Module
+    public static final int BACK_RIGHT_DRIVE_MOTOR_ID = 7;
+    public static final int BACK_RIGHT_STEER_MOTOR_ID = 8;
+    public static final int BACK_RIGHT_ENCODER_ID = 4;
+
+    // Shooter Motors
+    public static final int SHOOTER_MOTOR_1_ID = 9;
+    public static final int SHOOTER_MOTOR_2_ID = 10;
+    public static final int WRIST_MOTOR_ID = 11;
+}
--- src/main/java/frc/robot/constants/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/constants/shooter/ShooterConstants.java
@@ -1,16 +1,16 @@
 package frc.robot.constants.shooter;
 
-import com.ctre.phoenix6.configs.Slot1Configs;
-
+import com.ctre.phoenix6.configs.Slot0Configs;
 
 public class ShooterConstants
 {
-    public static final int SHOOTER_MOTOR_1_ID = 9;
-    public static final int SHOOTER_MOTOR_2_ID = 10;
-
     // Gains for the shooter motors
-    public static final Slot1Configs SHOOTER_GAINS = new Slot1Configs()
+    public static final Slot0Configs SHOOTER_GAINS = new Slot0Configs()
+        .withKP(0.35).withKI(0).withKD(0)
+        .withKS(0).withKV(0).withKA(0);
+    
+    // Gains for the wrist motor
+    public static final Slot0Configs WRIST_GAINS = new Slot0Configs()
         .withKP(0.1).withKI(0).withKD(0)
         .withKS(0).withKV(0).withKA(0);
-
 }
