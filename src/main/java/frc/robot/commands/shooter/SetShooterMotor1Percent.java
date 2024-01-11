fc0fe36e5eb28f37d1c20697e12c72ae770c8c74
Colin Metcalf <colinmetcalf@outlook.com>
2024-01-11 00:06:07 -0800

    Create commands

---
+++ src/main/java/frc/robot/commands/shooter/SetShooterMotor1Percent.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
+
+public class SetShooterMotor1Percent extends Command
+{
+  Shooter m_Shooter;
+  double m_Percent;
+  
+  /** Creates a new SetMotor1Velocity. */
+  public SetShooterMotor1Percent(Shooter shooter, double percent)
+  {
+    this.m_Shooter = shooter;
+    this.m_Percent = percent;
+    addRequirements(shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize()
+  {
+    // Intentionally Empty
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute()
+  {
+    m_Shooter.setShooterMotor1Percent(m_Percent);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted)
+  {
+    // Intentionally Empty
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished()
+  {
+    return false;
+  }
+}
--- src/main/java/frc/robot/commands/shooter/SetShooterMotor1Velocity.java
+++ src/main/java/frc/robot/commands/shooter/SetShooterMotor1Velocity.java
@@ -7,43 +7,44 @@ package frc.robot.commands.shooter;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shooter;
 
-public class SetVelocity extends Command {
-  private Shooter m_Shooter;
-  private double m_Velocity;
-  private int m_Motor;
-
-  /** Creates a new SetVelocity. */
-  public SetVelocity(Shooter shooter, double velocity, int motor) {
+public class SetShooterMotor1Velocity extends Command
+{
+  Shooter m_Shooter;
+  double m_Velocity;
+  
+  /** Creates a new SetMotor1Velocity. */
+  public SetShooterMotor1Velocity(Shooter shooter, double velocity)
+  {
     this.m_Shooter = shooter;
     this.m_Velocity = velocity;
-    this.m_Motor = motor;
     addRequirements(shooter);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize()
+  {
+    // Intentionally Empty
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    switch (m_Motor) {
-      case 1:
-        m_Shooter.setMotor1Velocity(m_Velocity);
-        break;
-      case 2:
-        m_Shooter.setMotor2Velocity(m_Velocity);
-        break;
-    }
+  public void execute()
+  {
+    m_Shooter.setMotor1Velocity(m_Velocity);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted)
+  {
+    // Intentionally Empty
+  }
 
   // Returns true when the command should end.
   @Override
-  public boolean isFinished() {
+  public boolean isFinished()
+  {
     return false;
   }
 }
