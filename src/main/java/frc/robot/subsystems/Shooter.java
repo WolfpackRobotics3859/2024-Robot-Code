6c06757da5da531bacd420a0241a96b2cc3afb7a
Colin Metcalf <colinmetcalf@outlook.com>
2024-01-11 00:05:23 -0800

    Create Subsystem

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,36 +4,87 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import frc.robot.constants.Hardware;
+import frc.robot.constants.shooter.ShooterConstants;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase
 {
-  private final TalonFX m_shooterMotor1 = new TalonFX(9);
-  private final TalonFX m_shooterMotor2 = new TalonFX(10);
+  private final TalonFX m_ShooterMotor1 = new TalonFX(Hardware.SHOOTER_MOTOR_1_ID);
+  private final TalonFX m_ShooterMotor2 = new TalonFX(Hardware.SHOOTER_MOTOR_2_ID);
+  private final TalonFX m_WristMotor = new TalonFX(Hardware.WRIST_MOTOR_ID);
 
   /** Creates a new Shooter. */
   public Shooter() 
   {
-    // Intentionally Empty
+    m_ShooterMotor1.getConfigurator().apply(ShooterConstants.SHOOTER_GAINS);
+    m_ShooterMotor2.getConfigurator().apply(ShooterConstants.SHOOTER_GAINS);
+    m_WristMotor.getConfigurator().apply(ShooterConstants.WRIST_GAINS);
   }
 
+  // Shooter Functions
   public void setMotor1Velocity(double rpm)
   {
-    MotionMagicVelocityVoltage request = new MotionMagicVelocityVoltage(rpm, 1, false, 0, 1, false, false, false);
-    m_shooterMotor1.setControl(request);
+    MotionMagicVelocityVoltage request = new MotionMagicVelocityVoltage(rpm, 40, false, 70, 0, false, false, false);
+    m_ShooterMotor1.setControl(request);
   }
 
   public void setMotor2Velocity(double rpm)
   {
-    MotionMagicVelocityVoltage request = new MotionMagicVelocityVoltage(rpm, 1, false, 0, 1, false, false, false);
-    m_shooterMotor2.setControl(request);
+    MotionMagicVelocityVoltage request = new MotionMagicVelocityVoltage(rpm, 1, false, 0, 0, false, false, false);
+    m_ShooterMotor2.setControl(request);
+  }
+
+  public void setShooterMotor1Percent(double percent)
+  {
+    DutyCycleOut request = new DutyCycleOut(percent, false, false, false, false);
+    m_ShooterMotor1.setControl(request);
+  }
+
+  public void setShooterMotor2Percent(double percent)
+  {
+    DutyCycleOut request = new DutyCycleOut(percent, false, false, false, false);
+    m_ShooterMotor2.setControl(request);
+  }
+
+  // Wrist Functions
+  public void setWristPos(double position)
+  {
+    PositionDutyCycle request = new PositionDutyCycle(position, 4, false, 0, 0, false, false, false);
+    m_WristMotor.setControl(request);
+  }
+
+  public void setWristPercent(double percent)
+  {
+    DutyCycleOut request = new DutyCycleOut(percent, false, false, false, false);
+    m_WristMotor.setControl(request);
+  }
+
+  // Telemetry Functions
+  public StatusSignal<Double> getShooterMotor1Velocity()
+  {
+    return m_ShooterMotor1.getVelocity();
+  }
+
+  public StatusSignal<Double> getShooterMotor2Velocity()
+  {
+    return m_ShooterMotor2.getVelocity();
+  }
+
+  public StatusSignal<Double> getWristMotorPos()
+  {
+    return m_ShooterMotor1.getPosition();
   }
 
   @Override
-  public void periodic() 
+  public void periodic()
   {
     // Intentionally Empty
   }
