29a10f3e1f8ef9d3fd41ff5c69207aa7258e2a04
Colin Metcalf <colinmetcalf@outlook.com>
2024-01-11 18:32:20 -0800

    Update shooter gains

--- src/main/java/frc/robot/constants/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/constants/shooter/ShooterConstants.java
@@ -6,11 +6,11 @@ public class ShooterConstants
 {
     // Gains for the shooter motors
     public static final Slot0Configs SHOOTER_GAINS = new Slot0Configs()
-        .withKP(0.35).withKI(0).withKD(0)
-        .withKS(0).withKV(0).withKA(0);
+        .withKP(0.015365).withKI(0).withKD(0)
+        .withKS(0.035335).withKV(0.11133).withKA(0.0098933);
     
     // Gains for the wrist motor
     public static final Slot0Configs WRIST_GAINS = new Slot0Configs()
-        .withKP(0.1).withKI(0).withKD(0)
+        .withKP(0.03).withKI(0).withKD(0)
         .withKS(0).withKV(0).withKA(0);
 }
