109a0b6172c2f056e114c5531277cccb11a38224
Colin Metcalf <colinmetcalf@outlook.com>
2024-01-12 15:21:38 -0800

    Subsystem Declaration

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,21 +9,31 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.constants.drivetrain.TunerConstants;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Elevator;
 import frc.robot.commands.drivetrain.Drive;
 import frc.robot.commands.drivetrain.SeedFieldRelative;
 
 public class RobotContainer 
 {
+  // Subsystems
   private final Drivetrain m_Drivetrain = new Drivetrain(TunerConstants.DRIVETRAIN_CONSTANTS, TunerConstants.FRONT_LEFT,
                       TunerConstants.FRONT_RIGHT, TunerConstants.BACK_LEFT, TunerConstants.BACK_RIGHT);
+  private final Elevator m_Elevator = new Elevator();
 
+  // Controllers
   private final CommandXboxController primaryController = new CommandXboxController(0);
 
+  // Getters
   public Drivetrain getDriveSub()
   {
     return this.m_Drivetrain;
   }
-  
+
+  public Elevator getElevator()
+  {
+    return this.m_Elevator;
+  }
+
   private void configureBindings() 
   {
     m_Drivetrain.setDefaultCommand(
