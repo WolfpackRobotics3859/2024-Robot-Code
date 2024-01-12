383a6a03e35a38b272bcd506f297f135a9b3af2e
Colin Metcalf <colinmetcalf@outlook.com>
2024-01-12 10:01:55 -0800

    Create subsystem functions

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,25 +4,57 @@
 
 package frc.robot.subsystems;
 
+import javax.swing.text.Position;
+
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.DutyCycle;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.elevator.ElevatorConstants;
 
 public class Elevator extends SubsystemBase
 {
-
   private final TalonFX m_ElevatorMotor1 = new TalonFX(Hardware.ELEVATOR_MOTOR_1_ID);
   private final TalonFX m_ElevatorMotor2 = new TalonFX(Hardware.ELEVATOR_MOTOR_2_ID);
 
   /** Creates a new Elevator. */
   public Elevator()
   {
-    m_ElevatorMotor1.getConfigurator().apply(ELEVATOR_GAINS);
-    m_ElevatorMotor2.getConfigurator().apply(ELEVATOR_GAINS);
+    // Apply gains to both motors
+    m_ElevatorMotor1.getConfigurator().apply(ElevatorConstants.ELEVATOR_GAINS);
+    m_ElevatorMotor2.getConfigurator().apply(ElevatorConstants.ELEVATOR_GAINS);
+
+    // Applies the software position limit to the first motor
+    m_ElevatorMotor1.getConfigurator().apply(ElevatorConstants.SOFT_LIMIT_CONFIGS);
     
+    // Applies limit switch configuration to the first motor
+    m_ElevatorMotor1.getConfigurator().apply(ElevatorConstants.HARD_LIMIT_CONFIGS);
+    
+    // Send a request to the second motor to follow the first
     Follower followRequest = new Follower(Hardware.ELEVATOR_MOTOR_1_ID, false);
     m_ElevatorMotor2.setControl(followRequest);
+  }
+
+  public void setElevatorPosition(double position)
+  {
+    PositionDutyCycle request = new PositionDutyCycle(position, ElevatorConstants.ELEVATOR_VELOCITY, false, 0, 0, false, false, false);
+    m_ElevatorMotor1.setControl(request);
+  }
+
+  public void setElevatorPercent(double percent)
+  {
+    DutyCycleOut request = new DutyCycleOut(percent, false, false, false, false);
+    m_ElevatorMotor1.setControl(request);
+  }
+
+  public StatusSignal<Double> getElevatorPosition()
+  {
+    return m_ElevatorMotor1.getPosition();
   }
 
   @Override
