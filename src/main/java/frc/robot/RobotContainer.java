2aa3aa51558e221ca12845248dab6d2c5dc9b93c
Colin Metcalf <96217596+coolinm@users.noreply.github.com>
2024-01-13 15:00:21 -0800

    15: Implement Shooter Subsystem
    * Basic shooter system
    
    * Create Subsystem
    
    * Create Constants
    
    * Update vendordeps
    
    * Create commands
    
    * Add Documentation
    
    * Update Documentation
    
    * Remove SysID
    
    * Update shooter gains
    
    * Fix Shooter Commands
    
    * Fix Commands
    
    * Create Shooter Object
    
    * Remove Seed Command
    
    * Move Constructor
    
    * Add Gains For Motor 2
    
    * This
    
    * Update Commands
    
    * Update Hardware Constants
    
    * Update Constants

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,11 +7,12 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.constants.Hardware;
 import frc.robot.constants.drivetrain.TunerConstants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Shooter;
 import frc.robot.commands.drivetrain.Drive;
-import frc.robot.commands.drivetrain.SeedFieldRelative;
 
 public class RobotContainer 
 {
@@ -19,21 +20,45 @@ public class RobotContainer
   private final Drivetrain m_Drivetrain = new Drivetrain(TunerConstants.DRIVETRAIN_CONSTANTS, TunerConstants.FRONT_LEFT,
                       TunerConstants.FRONT_RIGHT, TunerConstants.BACK_LEFT, TunerConstants.BACK_RIGHT);
   private final Elevator m_Elevator = new Elevator();
+  private final Shooter m_Shooter = new Shooter();
 
   // Controllers
-  private final CommandXboxController primaryController = new CommandXboxController(0);
+  private final CommandXboxController primaryController = new CommandXboxController(Hardware.PRIMARY_CONTROLLER_PORT);
 
   // Getters
+
+  /**
+   * @brief Gets the drivetrain subsystem.
+   * @return The drivetrain object
+   */
   public Drivetrain getDriveSub()
   {
     return this.m_Drivetrain;
   }
 
+  /**
+   * @brief Gets the elevator subsystem.
+   * @return The elevator object
+   */
   public Elevator getElevator()
   {
     return this.m_Elevator;
   }
 
+  /**
+   * @brief Gets the shooter subsystem.
+   * @return The shooter object
+   */
+  public Shooter getShooter()
+  {
+    return this.m_Shooter;
+  }
+
+  public RobotContainer() 
+  {
+    configureBindings();
+  }
+  
   private void configureBindings() 
   {
     m_Drivetrain.setDefaultCommand(
@@ -42,13 +67,6 @@ public class RobotContainer
                 () -> -primaryController.getLeftX(),
                 () -> -primaryController.getRightX()
       ));
-
-    primaryController.a().onTrue(new SeedFieldRelative(m_Drivetrain));
-  }
-
-  public RobotContainer() 
-  {
-    configureBindings();
   }
 
   public Command getAutonomousCommand() 
