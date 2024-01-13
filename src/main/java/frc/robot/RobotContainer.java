312183ad726e85f7b3355792269fc912bc851dbc
Colin Metcalf <96217596+coolinm@users.noreply.github.com>
2024-01-13 14:38:48 -0800

    11 - Implement Shooter Subsystem
    * 2: Implement Drivetrain Subsystem
    
    * Implement Drivetrain Subsystem
    
    * Update Field Relative Seed
    
    * Remove Telemetry
    
    * Remove Field Relative Seeding
    
    * Add Drivetrain Constants
    
    * Move Drivetrain Declaration
    
    * Documentation
    
    * Documentation
    
    * Add Commands
    
    * Update Robot Container
    
    * Documentation
    
    * Fix syntax
    
    * Move Drivetrain To Parameter
    Change Class Capitalization
    
    * Fix syntax
    
    * Update local variables
    
    * Fix Field Relative
    
    * Add Drivetrain Getter
    
    * Rename Drive.java
    
    * Basic shooter system
    
    * Create Subsystem
    
    * Create Constants
    
    * Update vendordeps
    
    * Create commands
    
    * Add Documentation
    
    * Update Documentation
    
    * Remove SysID
    
    * Update shooter gains
    
    * Fix Shooter Commands
    
    * Fix Commands
    
    * Create Shooter Object
    
    * Remove Seed Command
    
    * Move Constructor
    
    * Add Gains For Motor 2
    
    * This
    
    * Update Commands
    
    * Update Hardware Constants
    
    * Update Constants

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,15 +6,58 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.constants.Hardware;
+import frc.robot.constants.drivetrain.TunerConstants;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Shooter;
+import frc.robot.commands.drivetrain.Drive;
 
-public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
+public class RobotContainer 
+{
+  // Subsystems
+  private final Drivetrain m_Drivetrain = new Drivetrain(TunerConstants.DRIVETRAIN_CONSTANTS, TunerConstants.FRONT_LEFT,
+                      TunerConstants.FRONT_RIGHT, TunerConstants.BACK_LEFT, TunerConstants.BACK_RIGHT);
+  private final Shooter m_Shooter = new Shooter();
+  
+  // Controllers
+  private final CommandXboxController primaryController = new CommandXboxController(Hardware.PRIMARY_CONTROLLER_PORT);
+
+  /**
+   * @brief Gets the drivetrain subsystem.
+   * @return The drivetrain object
+   */
+  public Drivetrain getDriveSub()
+  {
+    return this.m_Drivetrain;
   }
 
-  private void configureBindings() {}
+  /**
+   * @brief Gets the shooter subsystem.
+   * @return The shooter object
+   */
+  public Shooter getShooter()
+  {
+    return this.m_Shooter;
+  }
 
-  public Command getAutonomousCommand() {
+  public RobotContainer() 
+  {
+    configureBindings();
+  }
+  
+  private void configureBindings() 
+  {
+    m_Drivetrain.setDefaultCommand(
+      new Drive(m_Drivetrain,
+                () -> -primaryController.getLeftY(),
+                () -> -primaryController.getLeftX(),
+                () -> -primaryController.getRightX()
+      ));
+  }
+
+  public Command getAutonomousCommand() 
+  {
     return Commands.print("No autonomous command configured");
   }
 }
---
+++ src/main/java/frc/robot/constants/shooter/ShooterConstants.java
@@ -0,0 +1,33 @@
+package frc.robot.constants.shooter;
+
+import com.ctre.phoenix6.configs.Slot0Configs;
+
+public class ShooterConstants
+{
+    // Shooter motor 1
+    public static final Slot0Configs SHOOTER_1_GAINS = new Slot0Configs()
+        .withKP(0.015365).withKI(0).withKD(0)
+        .withKS(0.035335).withKV(0.11133).withKA(0.0098933);
+
+    // Shooter motor 2
+    public static final Slot0Configs SHOOTER_2_GAINS = new Slot0Configs()
+        .withKP(0.015365).withKI(0).withKD(0)
+        .withKS(0.035335).withKV(0.11133).withKA(0.0098933);
+    
+    // Wrist motor
+    public static final Slot0Configs WRIST_GAINS = new Slot0Configs()
+        .withKP(0.03).withKI(0).withKD(0)
+        .withKS(0).withKV(0).withKA(0);
+    
+    public static final double WRIST_VELOCITY = 4;
+
+    public static final double SHOOTER_MOTOR_ACCELERATION = 40;
+
+    // Motor Enum
+    public enum MOTOR
+    {
+        MOTOR_1,
+        MOTOR_2,
+        WRIST_MOTOR
+    }
+}
