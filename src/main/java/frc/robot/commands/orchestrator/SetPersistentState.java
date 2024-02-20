179eb7b9feb7e697784373a71d9e8b634acab284
Colin Metcalf <colinmetcalf@outlook.com>
2024-02-19 16:34:22 -0800

    Rename Commands

--- src/main/java/frc/robot/commands/orchestrator/SetPersistentState.java
+++ src/main/java/frc/robot/commands/orchestrator/SetPersistentState.java
@@ -6,23 +6,23 @@ package frc.robot.commands.orchestrator;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Orchestrator;
-import frc.robot.subsystems.Orchestrator.OrchestratorAction;
+import frc.robot.subsystems.Orchestrator.RobotState;
 
-public class SetPersistentAction extends InstantCommand {
+public class SetPersistentState extends InstantCommand {
   private final Orchestrator m_Orchestrator;
-  private final OrchestratorAction m_DesiredAction;
+  private final RobotState m_DesiredState;
 
   /**
-   * Tells the orchestrator to perform an action. This request for an action will persist even after the controller's button is released.
-   * For example, if you want to tell the orchestrator to begin intaking, using this command you would only press the button once, but the
+   * Tells the orchestrator to move the robot to a certain state. This request for an state will persist even after the controller's button is released.
+   * For example, if you want to tell the robot to begin intaking, using this command you would only press the button once, but the
    * robot will continue intaking.
    * @param orchestrator The orchestrator subsystem.
-   * @param action The action you want the orchestrator to perform.
+   * @param action The state you want the robot to be in.
    */
-  public SetPersistentAction(Orchestrator orchestrator, OrchestratorAction desiredAction)
+  public SetPersistentState(Orchestrator orchestrator, RobotState desiredState)
   {
     this.m_Orchestrator = orchestrator;
-    this.m_DesiredAction = desiredAction;
+    this.m_DesiredState = desiredState;
 
     // add requirements to make it so that multiple commands cannot be requested at once
     addRequirements(m_Orchestrator);
@@ -32,6 +32,6 @@ public class SetPersistentAction extends InstantCommand {
   @Override
   public void initialize() 
   {
-    m_Orchestrator.setOrchestratorAction(this.m_DesiredAction);
+    m_Orchestrator.setOrchestratorState(m_DesiredState);;
   }
 }
--- src/main/java/frc/robot/commands/orchestrator/SetTemporaryState.java
+++ src/main/java/frc/robot/commands/orchestrator/SetTemporaryState.java
@@ -6,22 +6,22 @@ package frc.robot.commands.orchestrator;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Orchestrator;
-import frc.robot.subsystems.Orchestrator.OrchestratorAction;
+import frc.robot.subsystems.Orchestrator.RobotState;;
 
-public class SetTemporaryAction extends Command {
+public class SetTemporaryState extends Command {
   private final Orchestrator m_Orchestrator;
-  private final OrchestratorAction m_Action;
+  private final RobotState m_desiredState;
 
-  /** Tells the orchestrator to perform the requested action. This action will stop once the controller's button
+  /** Tells the orchestrator to move the robot to a given state. The robot will return to it's idle state once the controller's button
    * is released. For example, if you want the orchestrator to shoot, pressing the button will get the robot ready to 
-   * shoot, but once the button is release, the robot will stop preparing to shoot and go back to it's default state.
+   * shoot, but once the button is release, the robot will stop preparing to shoot and go back to it's idle state.
    * @param orchestrator The orchestrator subsystem
-   * @param action The action you want the robot to perform while the button is being pressed
+   * @param action The state you want the robot to be in while the button is being pressed
    */
-  public SetTemporaryAction(Orchestrator orchestrator, OrchestratorAction action)
+  public SetTemporaryState(Orchestrator orchestrator, RobotState desiredState)
   {
     this.m_Orchestrator = orchestrator;
-    this.m_Action = action;
+    this.m_desiredState = desiredState;
 
     // add requirements to make it so that multiple commands cannot be requested at once
     addRequirements(m_Orchestrator);
@@ -39,7 +39,7 @@ public class SetTemporaryAction extends Command {
   public void execute()
   {
     // Set orchestrator's action
-    m_Orchestrator.setOrchestratorAction(m_Action);
+    m_Orchestrator.setOrchestratorState(m_desiredState);
   }
 
   // Called once the command ends or is interrupted.
@@ -47,7 +47,7 @@ public class SetTemporaryAction extends Command {
   public void end(boolean interrupted)
   {
     // Remove the set action when button is released
-    m_Orchestrator.setOrchestratorAction(OrchestratorAction.NONE);
+    m_Orchestrator.setOrchestratorState(RobotState.IDLE);
   }
 
   // Returns true when the command should end.
