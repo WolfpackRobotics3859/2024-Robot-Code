c7c15869cabcd8eab3a9f2c033970901a941d946
Colin Metcalf <colinmetcalf@outlook.com>
2024-02-29 14:17:03 -0800

    Adjust swerve gains

--- src/main/java/frc/robot/constants/drivetrain/TunerConstants.java
+++ src/main/java/frc/robot/constants/drivetrain/TunerConstants.java
@@ -21,7 +21,7 @@ public class TunerConstants
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs DRIVE_GAINS = new Slot0Configs()
-        .withKP(3).withKI(0).withKD(0)
+        .withKP(2).withKI(0).withKD(0)
         .withKS(0).withKV(0).withKA(0);
 
     // The closed-loop output type to use for the steer motors;
