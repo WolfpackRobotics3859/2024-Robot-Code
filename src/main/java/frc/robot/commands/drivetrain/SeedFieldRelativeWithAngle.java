d04f49004382f21bc15c0b7e66699dc66978fb22
Colin Metcalf <colinmetcalf@outlook.com>
2024-03-02 08:30:48 -0800

    Drivetrain updates

---
+++ src/main/java/frc/robot/commands/drivetrain/SeedFieldRelativeWithAngle.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.drivetrain;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Drivetrain;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class SeedFieldRelativeWithAngle extends InstantCommand
+{
+  private final Drivetrain m_Drivetrain;
+  private final double m_Angle;
+
+  public SeedFieldRelativeWithAngle(Drivetrain drivetrain, double angle)
+  {
+    this.m_Drivetrain = drivetrain;
+    this.m_Angle = angle;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize()
+  {
+    m_Drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(this.m_Angle)));
+  }
+}
