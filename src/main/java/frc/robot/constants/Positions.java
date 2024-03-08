4040afb8838ed8af85b75da74bc11f7f92500ccb
Jason SooHoo <jtsoohoo@comcast.net>
2024-03-07 22:58:37 -0800

    23: Bug Fixes and Logic Changes

--- src/main/java/frc/robot/constants/Positions.java
+++ src/main/java/frc/robot/constants/Positions.java
@@ -16,7 +16,7 @@ public class Positions
     public static final class INTAKING
     {
         public static final double INTAKE_ROLLER_VOLTAGE = -6;
-        public static final double INTAKE_WRIST_POSITION = 0.1;
+        public static final double INTAKE_WRIST_POSITION = 0.5;
         public static final double SHOOTER_ROLLER_1_VELOCITY = -30;
         public static final double SHOOTER_ROLLER_2_VELOCITY = -30;
         public static final double SHOOTER_FEEDER_VOLTAGE = -3;
@@ -28,10 +28,10 @@ public class Positions
     {
         public static final double INTAKE_ROLLER_VOLTAGE = 0;
         public static final double INTAKE_WRIST_POSITION = 3.0;
-        public static final double SHOOTER_ROLLER_1_VELOCITY = -40;
-        public static final double SHOOTER_ROLLER_2_VELOCITY = -40;
-        public static final double SHOOTER_FEEDER_VOLTAGE = -8;
-        public static final double SHOOTER_WRIST_ANGLE = 0.45;
-        public static final double ELEVATOR_POSITION = 0.67;
+        public static final double SHOOTER_ROLLER_1_VELOCITY = -30;
+        public static final double SHOOTER_ROLLER_2_VELOCITY = -30;
+        public static final double SHOOTER_FEEDER_VOLTAGE = -10;
+        public static final double SHOOTER_WRIST_ANGLE = 0.53;
+        public static final double ELEVATOR_POSITION = 0.83;
     }
 }
