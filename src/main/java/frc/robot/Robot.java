3db84c90e4c4f3c75dd96f770b678a6142ba8f57
KennethBot1 <colinmetcalf@outlook.com>
2024-07-09 20:23:16 -0700

    Remove orchestrator

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -77,7 +77,6 @@ public class Robot extends LoggedRobot
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-    m_robotContainer.startTeleopCommands();
   }
 
   @Override
